18f9eb11fe213a1ca4bb724e8fc05078ee24d19f
ihyeon1120 <j.ihyeon1120.dev@gmail.com>
2025-09-03 13:53:24 +0900

    PS

--- Graph/Tree/dist_of_vertices_1761.cpp
+++ Graph/Tree/dist_of_vertices_1761.cpp
@@ -13,41 +13,64 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-vector<pii> parent(40001, {0, 0});
-vector<bool> visited(40001, false);
-vector<int> d(40001, 0);
+const int MAXN = 40001;
+const int LOG_N = (int)ceil(log2(MAXN));
+
+vector<vector<int>> parent(MAXN, vector<int>(LOG_N, 0));
+vector<int> depth(MAXN, -1);
+vector<int> dist(MAXN, -1);
 vector<vector<pii>> graph;
 
-void dfs(int node, int depth) {
-    visited[node] = true;
-    d[node] = depth;
-    for (pii next : graph[node]) {
-        auto [next_node, dist] = next;
-        if (visited[next_node]) continue;
-        parent[next_node] = {node, dist}; 
-        dfs(next_node, depth + 1);
+void dfs(int node, int par, int dep, int d) {
+
+    depth[node] = dep;
+    dist[node] = d;
+    parent[node][0] = par;
+
+    for (pii i : graph[node]) {
+        auto [next, w] = i;
+        if (next == par) continue;
+        dfs(next, node, dep + 1, d + w);
     }
 }
 
-int lca(int node1, int node2) {
-    int ans = 0;
-    while (d[node1] != d[node2]) {
-        if (d[node1] > d[node2]) {
-            ans += parent[node1].second;
-            node1 = parent[node1].first;
-        }
-        else {
-            node2 = parent[node2].first;
-            ans += parent[node2].second;
+void create_table() {
+    // parent[x][y] => x의 2^y 부모 저장
+    for (int k = 1; k < LOG_N; k++) {
+        for (int i = 1; i < MAXN; i++) {
+            /*
+                희소배열 생성
+                parent[i][k-1] -> i의 2^k-1 부모
+                parent[parent[i][k-1]][k-1] -> i의 2^k-1 부모의 2^k-1 부모
+            */
+            parent[i][k] = parent[parent[i][k-1]][k-1];
         }
     }
-    while (node1 != node2) {
-        ans += parent[node1].second;
-        node1 = parent[node1].first;
-        ans += parent[node2].second;
-        node2 = parent[node2].first;
+}
+
+int lca(int u, int v) {
+    // u가 더 깊게
+    if (depth[u] < depth[v])
+        swap(u, v);
+    // u와 v의 깊이를 맞추는 반복문
+    for (int k = LOG_N - 1; k >= 0; k--) {
+        // u가 v보다 더 깊거나 같지만 점프뛸 수 있는 최대로 뛰기
+        if (depth[u] - (1 << k) >= depth[v]) {
+            u = parent[u][k];
+        }
     }
-    return ans;
+
+    if (u == v) return u;
+
+    for (int k = LOG_N - 1; k >= 0; k--) {
+        // 조상이 아직까지 다르다면 점프
+        if (parent[u][k] != parent[v][k]) {
+            u = parent[u][k];
+            v = parent[v][k];
+        }
+    }
+
+    return parent[u][0];
 }
 
 void solve() {
@@ -59,12 +82,13 @@ void solve() {
         graph[a].push_back({b, c});
         graph[b].push_back({a, c});
     }
-    dfs(1, 0);
+    dfs(1, 0, 0, 0);
+    create_table();
     int M; cin >> M;
     while (M--)
     {
         int a, b; cin >> a >> b;
-        cout << lca(a, b) << endl;
+        cout << dist[a] + dist[b] - (2 * dist[lca(a, b)]) << endl;
     }
     
 }
